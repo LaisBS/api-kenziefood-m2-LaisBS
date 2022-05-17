fb0ebd40cb5536ec8700549bd7b5db7600cd1d5b
LaisBS <96962027+LaisBS@users.noreply.github.com>
2022-05-17 15:39:39 -0300

    Feature: listar_produtos

---
+++ scripts/controllers/products-controller.js
@@ -0,0 +1,89 @@
+class ProductsController {
+    static BASE_URL = "https://api-kenzie-food.herokuapp.com/my/products/";
+    static token = localStorage.getItem("Token");
+  
+    static async getPublicProducts() {
+      let data=await fetch("https://api-kenzie-food.herokuapp.com/products/", {
+        method: "GET",
+        // method: {
+        //   "Content-Type": "application/json"
+        // }
+      })
+      .then((response) => {   
+        return response.json();
+        
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+      console.log(data)
+      return data
+    }
+  
+    static async getPrivateProducts() {
+      return await fetch(this.BASE_URL, {
+        method: "GET",
+        method: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${this.token}`
+        }
+      })
+      .then((response) => {
+        return response.json();
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    }
+  
+    static async createProduct(productData) {
+      return await fetch(this.BASE_URL+"products", {
+        method: "POST",
+        method: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${this.token}`
+        },
+        body: JSON.stringify(productData)
+      })
+      .then((response) => {
+        return response.status;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    }
+  
+    static async changeProduct(productData) {
+      return await fetch(this.BASE_URL+"products", {
+        method: "PATCH",
+        method: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${this.token}`
+        },
+        body: JSON.stringify(productData)
+      })
+      .then((response) => {
+        return response.status;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    }
+  
+    static async removeProduct(productId) {
+      return await fetch(this.BASE_URL+productId, {
+        method: "POST",
+        method: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${this.token}`
+        }
+      })
+      .then((response) => {
+        return response.status;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    }
+  }
+  export default ProductsController
