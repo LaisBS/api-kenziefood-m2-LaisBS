748150e2290b14fc052467b69ec5f2ec20e79420
LaisBS <96962027+LaisBS@users.noreply.github.com>
2022-05-18 18:04:54 -0300

    Feat:Pagina_dashboard_DOM

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -5,3 +5,46 @@ import ProductsController from "/scripts/controllers/products-controller.js"
 let response=await ProductsController.getPrivateProducts()
 
 console.log(response)
+
+class myproducts{
+    static products(response){
+        const ul=document.getElementById("ul")
+
+        response.forEach(product => {
+            const li =document.createElement("li");
+            const div =document.createElement("div");
+            div.classList.add("product")
+            const img =document.createElement("img");
+            img.setAttribute("class","photo")
+            const h3 =document.createElement("h3");
+            const p=document.createElement("p")
+            const pd=document.createElement("p")
+            const divB =document.createElement("div");
+            divB.setAttribute("id","actions")
+            const buttonE=document.createElement("img");
+            buttonE.setAttribute("id","btnEdit");
+            const buttonD=document.createElement("img");
+            buttonD.setAttribute("id","btnDelete")
+
+            img.src=product.imagem
+            h3.innerText=product.nome
+            p.innerText=product.categoria
+            pd.innerText=product.descricao
+            buttonE.src="/images/EDIT.svg"
+            buttonD.src="/images/trash.svg"
+
+            div.appendChild(img)
+            div.appendChild(h3)
+            div.appendChild(p)
+            div.appendChild(pd)
+            divB.appendChild(buttonE)
+            divB.appendChild(buttonD)
+
+            div.appendChild(divB)
+            li.appendChild(div)
+
+            ul.appendChild(li)
+        });
+    }
+}
+myproducts.products(response)
